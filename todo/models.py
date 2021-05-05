dc6e4a47499ef32f3acd62ee5e35185f70158359
DeeAnnEye <danielashulamithjustin@gmail.com>
2021-05-05 16:47:06 +0530

    Add Admin panel

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Task(models.Model):
-    task_text = models.CharField(max_length = 200)
-    task_date = models.DateTimeField('task date')
+    task_text = models.CharField(max_length=200)
+    task_date = models.DateField('task date')
