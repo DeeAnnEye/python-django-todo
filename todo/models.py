aeb62cec6bc7530b0a69bab687cb0d59cf81c022
DeeAnnEye <danielashulamithjustin@gmail.com>
2021-05-05 16:15:38 +0530

    Create db and add model Task

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    task_text = models.CharField(max_length = 200)
+    task_date = models.DateTimeField('task date')
