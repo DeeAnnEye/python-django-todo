c31b45c3f9cc3eed6fd1589d6739e640c4701214
DeeAnnEye <danielashulamithjustin@gmail.com>
2021-05-06 06:02:56 +0530

    Add templates and import render in views.py

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # ex: /tasks/
+    path('tasks/', views.tasks, name='tasks'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,20 @@
 # from django.shortcuts import render
 
 from django.http import HttpResponse
+# from django.template import loader
+from django.shortcuts import render
 
+from .models import Task
 
 def index(request):
     return HttpResponse("Hello, world. You're at the todo index.")
+
+def tasks(request):
+    todo_list = Task.objects.order_by('-task_date')[:5]
+    # template = loader.get_template('todo/index.html')
+    # context = {
+    #     'todo_list': todo_list,
+    # }
+    # return HttpResponse(template.render(context, request))
+    context = {'todo_list': todo_list}
+    return render(request, 'todo/index.html', context)
