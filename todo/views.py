a4ac73c80a7ed8d9eb1e27a942d72228f24ecca1
DeeAnnEye <danielashulamithjustin@gmail.com>
2021-05-21 07:35:36 +0530

    Changed views.py

--- todo/views.py
+++ todo/views.py
@@ -11,12 +11,14 @@ def index(request):
 
 def tasks(request):
     todo_list = Task.objects.order_by('task_text')
+    for e in Task.objects.all():
+      print(e.task_date)
     # template = loader.get_template('todo/index.html')
     # context = {
     #     'todo_list': todo_list,
     # }
     # return HttpResponse(template.render(context, request))
-    print(request.POST)
+    # print(request.POST)
     context = {'todo_list': todo_list}
     return render(request, 'todo/tasks.html', context)
 
