25321583f5ba404e08eb4bc79883897278ec6b5c
DeeAnnEye <danielashulamithjustin@gmail.com>
2021-05-22 05:47:07 +0530

    Add task_time in task model

--- todo/models.py
+++ todo/models.py
@@ -6,3 +6,4 @@ from django.db import models
 class Task(models.Model):
     task_text = models.CharField(max_length=200)
     task_date = models.CharField(max_length=200)
+    task_time = models.CharField(max_length=300)
